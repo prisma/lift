5af0ff97396f1954c231f5cb93365c4307b3c489
Akshay Patil <akshayjpatil11@gmail.com>
2019-09-24 20:13:16 -0400

    Updated the missingGeneratorMessage.ts

--- src/utils/generation/missingGeneratorMessage.ts
+++ src/utils/generation/missingGeneratorMessage.ts
@@ -4,7 +4,7 @@ import { highlightDatamodel } from '../../cli/highlight/highlight'
 export const missingGeneratorMessage = `\n${chalk.blue(
   'info',
 )} You don't have defined any generator in your ${chalk.bold(
-  'schema.prisma',
+  'prisma/schema.prisma',
 )}, so nothing will be generated. You can define them like this:
 
 ${chalk.bold(
