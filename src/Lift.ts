prisma/prisma

commit da8ab2de2af8280003898fc22c60b5af53d14c94

support <inc|name|timestamp> in up/down cli (#156)

src/Lift.ts
@@ -49,14 +49,14 @@ const exists = promisify(fs.exists)
 
 export interface UpOptions {
   preview?: boolean
-  n?: number
+  n?: string
   short?: boolean
   verbose?: boolean
   autoApprove?: boolean
   onWarnings?: (warnings: EngineResults.Warning[]) => Promise<boolean>
 }
 export interface DownOptions {
-  n?: number
+  n?: string
 }
 
 export interface WatchOptions {
@@ -443,11 +443,12 @@ export class Lift {
     return ''
   }
 
-  public async down({ n }: DownOptions): Promise<string> {
+  public async down({ n = '1' }: DownOptions): Promise<string> {
     await this.getLockFile()
     const before = Date.now()
-    const localMigrations = await this.getLocalMigrations()
+    const { localMigrations, appliedRemoteMigrations } = await this.getMigrationsToApply(undefined, String(n))
     const localWatchMigrations = await this.getLocalWatchMigrations()
+
     if (localWatchMigrations.length > 0) {
       throw new Error(
         `Before running ${chalk.yellow('prisma lift down')}, please save your ${chalk.bold(
@@ -457,8 +458,8 @@ export class Lift {
         )}`,
       )
     }
+
     const datamodel = await this.getDatamodel()
-    const appliedRemoteMigrations = await this.engine.listAppliedMigrations({ sourceConfig: datamodel })
 
     // TODO cleanup
     let lastAppliedIndex = -1
@@ -486,20 +487,16 @@ export class Lift {
       return 'No migration to roll back'
     }
 
-    if (n && n > appliedMigrations.length) {
+    if (!appliedMigrations.length) {
       throw new Error(
-        `You provided ${chalk.redBright(`n = ${chalk.bold(String(n))}`)}, but there are only ${
-          appliedMigrations.length
-        } applied migrations that can be rolled back. Please provide ${chalk.green(
-          String(appliedMigrations.length),
-        )} or lower.`,
+        `Your filter ${chalk.redBright(
+          `n = ${chalk.bold(String(n))}`,
+        )}, does not match any number of applied migrations that can be rolled back. Please provide a valid <dec|name|timestamp>.`,
       )
     }
 
-    n = n || 1
-
-    for (let i = 0; i < n; i++) {
-      const lastApplied = localMigrations[lastAppliedIndex]
+    for (let i = 0; i < appliedMigrations.length; i++) {
+      const lastApplied = appliedMigrations[lastAppliedIndex]
       console.log(`Rolling back migration ${blue(lastApplied.id)}`)
 
       const result = await this.engine.unapplyMigration({ sourceConfig: datamodel })
@@ -518,13 +515,8 @@ export class Lift {
     await this.getLockFile()
     const before = Date.now()
 
-    const migrationsToApplyResult = await this.getMigrationsToApply()
-    const { lastAppliedIndex, localMigrations, appliedRemoteMigrations, sourceConfig } = migrationsToApplyResult
-    let { migrationsToApply } = migrationsToApplyResult
-
-    if (typeof n === 'number') {
-      migrationsToApply = migrationsToApply.slice(0, n)
-    }
+    const migrationsToApplyResult = await this.getMigrationsToApply(String(n))
+    const { lastAppliedIndex, localMigrations, migrationsToApply, sourceConfig } = migrationsToApplyResult
 
     if (!short) {
       const previewStr = preview ? ` --preview` : ''
@@ -551,7 +543,11 @@ export class Lift {
 
     console.log(`\nChecking the datasource for potential data loss...`)
     const firstMigrationToApplyIndex = localMigrations.indexOf(migrationsToApply[0])
-    const migrationsWithDbSteps = await this.getDatabaseSteps(localMigrations, firstMigrationToApplyIndex, sourceConfig)
+    const migrationsWithDbSteps = await this.getDatabaseSteps(
+      migrationsToApply,
+      firstMigrationToApplyIndex,
+      sourceConfig,
+    )
 
     const warnings = migrationsWithDbSteps.flatMap(m => m.warnings)
 
@@ -789,7 +785,10 @@ export class Lift {
     return migrationsWithDatabaseSteps.slice(fromIndex)
   }
 
-  private async getMigrationsToApply(): Promise<{
+  private async getMigrationsToApply(
+    toApplyFilter?: string,
+    appliedFilter?: string,
+  ): Promise<{
     localMigrations: LocalMigration[]
     lastAppliedIndex: number
     migrationsToApply: LocalMigration[]
@@ -833,12 +832,27 @@ export class Lift {
       return true
     })
 
+    const filterMigrations = (values: any[], n: string): any[] => {
+      if (!n.length) {
+        return values
+      }
+
+      const maybeNumber = Number(n)
+      const isNumber = !Number.isNaN(maybeNumber)
+      const i = values.findIndex(({ id = '' }, index) => {
+        const [date, name] = id.split(/-/)
+        return date === n || name === n || (isNumber && maybeNumber === index + 1)
+      })
+
+      return values.slice(0, i + 1)
+    }
+
     return {
       localMigrations,
       lastAppliedIndex,
-      migrationsToApply,
-      appliedRemoteMigrations,
+      appliedRemoteMigrations: filterMigrations(appliedRemoteMigrations.reverse(), String(appliedFilter)).reverse(),
       sourceConfig,
+      migrationsToApply: filterMigrations(migrationsToApply, String(toApplyFilter)),
     }
   }
 }

src/cli/commands/LiftDown.ts
@@ -60,19 +60,8 @@ export class LiftDown implements Command {
 
     const lift = new Lift()
 
-    const options: DownOptions = {}
-
-    // TODO add go down by name and timestamp
-    if (args._.length > 0) {
-      const thisArg = args._[0]
-      const maybeNumber = parseInt(thisArg, 10)
-
-      // in this case it's a migration id
-      if (isNaN(maybeNumber) || typeof maybeNumber !== 'number') {
-        throw new Error(`Invalid migration step ${maybeNumber}`)
-      } else {
-        options.n = maybeNumber
-      }
+    const options: DownOptions = {
+      n: args._.pop(),
     }
 
     await ensureDatabaseExists('unapply', true)

src/cli/commands/LiftUp.ts
@@ -77,21 +77,14 @@ export class LiftUp implements Command {
       autoApprove: args['--auto-approve'],
     }
 
-    if (args._.length > 0) {
-      const thisArg = args._[0]
-      const maybeNumber = parseInt(thisArg, 10)
-
-      // in this case it's a migration id
-      if (isNaN(maybeNumber) || typeof maybeNumber !== 'number') {
-        throw new Error(`Invalid migration step ${maybeNumber}`)
-      } else {
-        options.n = maybeNumber
-      }
-    }
-
     await ensureDatabaseExists('apply', true, args['--create-db'])
 
-    const result = await lift.up(options)
+    const result = await lift.up(
+      Object.assign(options, {
+        n: args._.pop(),
+      }),
+    )
+
     lift.stop()
     return result
   }

prisma/lift

commit 3a2b554b8c1818d5672ef4c97eaea11651b57a0d

Fix filtering of migrations in lift up
String(undefined) === "undefined", not empty string.

The bug was introduced in da8ab2de2af8280003898fc22c60b5af53d14c94

src/Lift.ts
@@ -832,8 +832,8 @@ export class Lift {
       return true
     })
 
-    const filterMigrations = (values: any[], n: string): any[] => {
-      if (!n.length) {
+    const filterMigrations = (values: any[], n: string | undefined): any[] => {
+      if (!n) {
         return values
       }
 
@@ -850,9 +850,9 @@ export class Lift {
     return {
       localMigrations,
       lastAppliedIndex,
-      appliedRemoteMigrations: filterMigrations(appliedRemoteMigrations.reverse(), String(appliedFilter)).reverse(),
+      appliedRemoteMigrations: filterMigrations(appliedRemoteMigrations.reverse(), appliedFilter).reverse(),
       sourceConfig,
-      migrationsToApply: filterMigrations(migrationsToApply, String(toApplyFilter)),
+      migrationsToApply: filterMigrations(migrationsToApply, toApplyFilter),
     }
   }
 }